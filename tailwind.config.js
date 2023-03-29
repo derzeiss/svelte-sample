bddb54af4e94aa8bddbaa4f7eaca76f1a6f5d7b1
alex <alex.zeiss@gmx.net>
2023-03-29 13:59:41 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
